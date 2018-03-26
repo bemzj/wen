2a0b969df8de1912f13363b793d37a6b6216c79e
zhongRui_HE <418351897@qq.com>
2018-03-26 18:11:22 +0800

    xxxx

--- js/personList9.js
+++ js/personList9.js
@@ -1,4 +1,44 @@
 $(function () {
+    layui.use('laydate', function(){
+        var laydate = layui.laydate;
+
+        //执行一个laydate实例
+        laydate.render({
+            elem: '#date' //指定元素
+        });
+    });
+    layui.use('laydate', function(){
+        var laydate = layui.laydate;
+
+        //执行一个laydate实例
+        laydate.render({
+            elem: '#date1' //指定元素
+        });
+    });
+    layui.use('laydate', function(){
+        var laydate = layui.laydate;
+
+        //执行一个laydate实例
+        laydate.render({
+            elem: '#date2' //指定元素
+        });
+    });
+    layui.use('laydate', function(){
+        var laydate = layui.laydate;
+
+        //执行一个laydate实例
+        laydate.render({
+            elem: '#date3' //指定元素
+        });
+    });
+    layui.use('laydate', function(){
+        var laydate = layui.laydate;
+
+        //执行一个laydate实例
+        laydate.render({
+            elem: '#date4' //指定元素
+        });
+    });
 	//获取高度
 	var hs = $('.con-left').height();
 
@@ -28,13 +68,15 @@ $(function () {
 		}
 		$('.con-left').height(h);
 		$('.con-right').height(h);
-    })
+    });
+    // 弹出删除框
     $('.add-del').click(function () {
         $('#del').show()
-    })
+    });
+    //取消
     $('#canc').click(function () {
         $('#del').hide()
-    })
+    });
     $('#addman').click(function () {
   
         $('.renyuan').hide();
@@ -94,4 +136,17 @@ $(function () {
 		$('.con-left').height(h);
 		$('.con-right').height(h);
     })
+	//切换发票
+	$('#con-wrap .row .con-right .right-newFapiao .fp-list1 >div').click(function () {
+		$(this).siblings('div').removeClass('sel');
+		$(this).addClass('sel');
+		if($(this).index()<=1)
+		{
+			$('.qiye').show();
+            $('.geren').hide();
+		}else{
+			$('.geren').show();
+            $('.qiye').hide();
+		}
+    });
 })
