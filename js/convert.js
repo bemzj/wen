c03651461f33c40891114c7e6baa0c80d40e99b8
zhongRui_HE <418351897@qq.com>
2018-03-28 01:07:17 +0800

    person7

---
+++ js/convert.js
@@ -0,0 +1,35 @@
+$(function () {
+    $('#qh').click(function () {
+        $('#mask').show();
+    })
+    $('#mask').click(function () {
+
+        $(this).stop().hide();
+    })
+    //切换地址复选
+    var fx = true;
+    var fx1 = true;
+    $('#awesome1').click(function () {
+        console.log(1)
+        if(fx==true){
+            $('.list-1 div').addClass("check");
+            $(this).attr({"checked":"checked"})
+            fx = false;
+        }else{
+            $('.list-1 div').removeClass("check");
+            $(this).removeAttr("checked")
+            fx = true;
+        }
+    })
+    $('#awesome2').click(function () {
+        if(fx1==true){
+            $('.list-2 div').addClass("check");
+            $(this).attr({"checked":"checked"})
+            fx1 = false;
+        }else{
+            $('.list-2 div').removeClass("check");
+            $(this).removeAttr("checked")
+            fx1 = true;
+        }
+    })
+})
