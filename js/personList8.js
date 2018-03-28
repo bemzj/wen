ff042e361be7ba324a01d4a55587ab480dd1bca8
zhongRui_HE <418351897@qq.com>
2018-03-28 19:15:12 +0800

    person8

--- js/personList8.js
+++ js/personList8.js
@@ -26,15 +26,55 @@ $(function () {
         }
     })
     //提现弹框
-    $('.tixian').click(function () {
-        $('.tishi').show();
+    $('#tx').click(function () {
+        $('#tishi').show();
     })
     $('.wrap-3 button').click(function () {
-        $('.tishi').hide();
+        $('#tishi').hide();
     })
     //转出到现金余额
     $('#zhuanchu').click(function () {
+        // $('#zcje').show();
+        // $('#right-fx').hide();
         $('#tixian').show();
+    })
+    $('.wrap-3 button').click(function () {
+        $('#tixian').hide();
+        $('#tixian-suc').hide();
+    })
+    //转出成功
+    $('#zc').click(function () {
+        $('#tixian-suc').show();
+    })
+    //充值跳转
+    $('.chongzhi').click(function () {
+        $('.right-wrap').hide();
+        $('.right-cz').show();
+    })
+    //优惠券跳转
+    $('#youhuiquan').click(function () {
+        $('.right-wrap').hide();
+        $('.right-yhq').show();
+    })
+    //返现跳转
+    $('#fanxian').click(function () {
+        $('.right-wrap').hide();
+        $('#right-fx').show();
+    })
+    //切换到交易明细
+    $('#jymx').click(function () {
+        $('.right-cz').hide();
+        $('.right-jymx').show();
+    })
+    //切换到现金余额
+    $('#xjye').click(function () {
+        $('.right-wrap').hide();
+        $('.right-fx1').show();
+    })
+    //现金余额切换到提现金额
+    $('#tixian1').click(function () {
+        $('.right-fx1').hide();
+        $('#txje').show();
     })
     //tab切换
     $('.c2-1').find("div").click(function () {
@@ -57,4 +97,9 @@ $(function () {
         $(this).addClass("sel");
         $(this).siblings("div").removeClass("sel")
     })
+    //支付方式
+    $('.payThree a').click(function(){
+        $(this).addClass('payChoice');
+        $(this).siblings('a').removeClass('payChoice');
+    });
 })
